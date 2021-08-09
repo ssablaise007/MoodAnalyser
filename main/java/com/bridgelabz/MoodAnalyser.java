7fd39bfa7d6fd17b506a0df8cb870c67e5f21144
Your Name <you@example.com>
2021-08-09 16:43:29 +0530

    Added : Mood Analyser Test

--- main/java/com/bridgelabz/MoodAnalyser.java
+++ main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,19 +1,17 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
-
-    public static class MoodAnalyzer {
-
-        public String analyzeMood(String message) throws MoodAnalyzerException {
-
-            if(message == null || message.isEmpty()) {
-                throw new MoodAnalyzerException("Please enter proper message");
-            }
-
-            if(message.contains("Sad"))
-                return "SAD";
-            else
-                return "HAPPY";
+    public String analyseMood(String message) throws MoodAnalyserException {
+        if (message == null || message.isEmpty()) {
+            throw new MoodAnalyserException("Submit A name");
         }
+        if (message.contains("sad")) {
+            return "SAD";
+        } else {
+            return "HAPPY";
+        }
+    }
+
+    public class MoodAnalyzer {
     }
 }
--- test/java/com/bridgelabz/MoodAnalyserTest.java
+++ test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -4,17 +4,34 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MoodAnalyzerTest {
+    @Test
+    public void testMoodAnalyser() throws Exception, MoodAnalyserException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a sad message");
+        Assertions.assertEquals(mood, "HAPPY");
+    }
 
     @Test
-    public void givenNullMood_ShouldReturnHappy_Test() {
-        MoodAnalyser.MoodAnalyzer moodAnalyzer = new MoodAnalyser.MoodAnalyzer();
-        String mood = null;
+    public void happyMoodAnalyser() throws Exception, MoodAnalyserException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a happy message");
+        Assertions.assertEquals(mood, "HAPPY");
+    }
+
+    @Test
+    public void nullMoodAnalyser() throws Exception, MoodAnalyserException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood(null);
+        Assertions.assertEquals(mood, "SILAR");
+    }
+
+    @Test
+    public void throwMoodAnalyser_byuser() throws Exception {
         try {
-            mood = moodAnalyzer.analyzeMood("");
-            Assertions.assertEquals("HAPPY", mood);
-        } catch (MoodAnalyzerException ex) {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            String mood = moodAnalyser.analyseMood(null);
+            Assertions.assertEquals(mood, "SILAR");
+        } catch (MoodAnalyserException ex) {
             System.out.println(ex.getMessage());
-            ex.printStackTrace();
         }
     }
-}
